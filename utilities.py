186fc92292262de27b17e45a0f69931106a8cd64
ddvlamin <ddvlamin@gmail.com>
2020-06-11 22:28:23 +0200

    utilities

--- utilities.py
+++ utilities.py
@@ -1,6 +1,10 @@
 from itertools import product
+from collections import Counter
+
+import numpy as np
 
 NUCLEOTIDES = "ACGT"
+nucl2ind = {nucleotide: i for i, nucleotide in enumerate(NUCLEOTIDES)}
 
 def generate_all_kmers(k):
   for pattern_tuple in product(NUCLEOTIDES, repeat=k):
@@ -43,3 +47,40 @@ def kmer_neighbors(kmer, distance):
              neighbors[kmer[0] + neighbor] = 0
     
     return neighbors
+
+def create_profile(motifs):
+  profile = np.zeros((4,len(motifs[0])))
+  for coli in range(len(motifs[0])):
+    for rowi in range(len(motifs)):
+      i = nucl2ind[motifs[rowi][coli]]
+      profile[i, coli] += 1/len(motifs)
+  return profile
+
+def create_laplace_profile(motifs):
+  profile = np.ones((4,len(motifs[0])))
+  for coli in range(len(motifs[0])):
+    for rowi in range(len(motifs)):
+      i = nucl2ind[motifs[rowi][coli]]
+      profile[i, coli] += 1/len(motifs)
+  return profile
+
+def profile_most_probable_kmer(dna_string, k, profile):
+  max_prob = 0
+  most_prob_kmer = dna_string[0:k]
+  for kmer in generate_kmers(dna_string, k):
+    prob = np.prod([profile[nucl2ind[nucl],i] for i, nucl in enumerate(kmer)])
+    if prob > max_prob:
+      max_prob = prob
+      most_prob_kmer = kmer
+  return most_prob_kmer
+
+def score_motifs(motifs):
+  score = 0
+  for coli in range(len(motifs[0])):
+    c = Counter()
+    for rowi in range(len(motifs)):    
+      c[motifs[rowi][coli]] += 1
+    most_frequent_nucl = max(c.items(), key=lambda x: x[1])[0]
+    c.pop(most_frequent_nucl)
+    score += sum(c.values())
+  return score
